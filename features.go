d62132de3ff5d785b6c030d1bab7da9263e50aee
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2022-11-21 04:07:03 -0500

    features: Add support for chat/convo services and help text

--- features.go
+++ features.go
@@ -2,31 +2,48 @@ package features
 
 import (
 	"github.com/Clinet/clinet_cmds"
+	"github.com/Clinet/clinet_convos"
+	"github.com/Clinet/clinet_services"
 )
 
 type FeatureMap struct {
-	Features []*Feature `json:"features"`
+	Features []Feature `json:"features"`
 }
-var featureMap *FeatureMap
+var FM *FeatureMap
 
 type Feature struct {
-	Name   string       `json:"name"`
-	Toggle bool         `json:"toggle"`
-	Cmds   []*cmds.Cmd  `json:"-"`
-	Init   func() error `json:"-"`
+	Toggle       bool                `json:"toggle"`
+	Name         string              `json:"name"`
+	Help         string              `json:"-"`
+	Init         func() error        `json:"-"`
+	Cmds         []*cmds.Cmd         `json:"-"`
+	ServiceChat  services.Service    `json:"-"`
+	ServiceConvo convos.ConvoService `json:"-"`
 }
 
-func SetFeatures(features []*Feature) {
-	if featureMap == nil {
-		featureMap = &FeatureMap{}
+func SetFeatures(features []Feature) {
+	if FM == nil {
+		FM = &FeatureMap{}
 	}
-	featureMap.Features = features
+	FM.Features = features
 }
 func IsEnabled(feature string) bool {
-	for _, f := range featureMap.Features {
+	for _, f := range FM.Features {
 		if feature == f.Name {
 			return f.Toggle
 		}
 	}
 	return false
+}
+func UpdateFeature(feature Feature) {
+	if FM != nil {
+		if FM.Features != nil && len(FM.Features) > 0 {
+			for i := 0; i < len(FM.Features); i++ {
+				if FM.Features[i].Name == feature.Name {
+					feature.Toggle = FM.Features[i].Toggle
+					FM.Features[i] = feature
+				}
+			}
+		}
+	}
 }
